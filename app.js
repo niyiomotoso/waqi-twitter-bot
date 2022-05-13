37c1d6794dce1f93e1a3e795e7f1f54e594cb6a5
Niyi Omotoso <omoniyiomotoso@gmail.com>
2022-05-13 02:05:20 +0200

    handle midmap for larger group

--- app.js
+++ app.js
@@ -1,1 +1,1 @@
-formatHashTagText([cityAndCountry.cityName, cityAndCountry.countryName]); await sendTextOnlyTweet(fullMessage); await sendTextAndMediaTweet(fullMessage, imageBuffer); Fri May 13 01:41:16 2022 +0200
+formatHashTagText([cityAndCountry.cityName, cityAndCountry.countryName]); await sendTextOnlyTweet(fullMessage); await sendTextAndMediaTweet(fullMessage, imageBuffer); Fri May 13 02:05:20 2022 +0200
