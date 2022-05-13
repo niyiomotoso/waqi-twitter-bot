815475e4628fd4b6614a2c10a17ba278bbe0cced
Niyi Omotoso <omoniyiomotoso@gmail.com>
2022-05-13 02:05:05 +0200

    add single-city support

--- app.js
+++ app.js
@@ -1,1 +1,1 @@
-formatHashTagText([cityAndCountry.cityName, cityAndCountry.countryName]); await sendTextOnlyTweet(fullMessage); await sendTextAndMediaTweet(fullMessage, imageBuffer); Fri May 13 02:03:58 2022 +0200
+formatHashTagText([cityAndCountry.cityName, cityAndCountry.countryName]); await sendTextOnlyTweet(fullMessage); await sendTextAndMediaTweet(fullMessage, imageBuffer); Fri May 13 02:05:05 2022 +0200
