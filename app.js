f1889cc95ea08f5ceb62caeedbbb90be71037238
Niyi Omotoso <omoniyiomotoso@gmail.com>
2022-05-25 13:37:04 +0200

    Update dependencies to latest versions

--- app.js
+++ app.js
@@ -1,1 +1,1 @@
-formatHashTagText([cityAndCountry.cityName, cityAndCountry.countryName]); await sendTextOnlyTweet(fullMessage); await sendTextAndMediaTweet(fullMessage, imageBuffer); Fri May 13 01:28:26 2022 +0200
+formatHashTagText([cityAndCountry.cityName, cityAndCountry.countryName]); await sendTextOnlyTweet(fullMessage); await sendTextAndMediaTweet(fullMessage, imageBuffer); Wed May 25 13:37:04 2022 +0200
