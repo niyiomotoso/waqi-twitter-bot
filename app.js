bb00329cfcd3685d57a8d68026bbeb6b59ef05bc
Niyi Omotoso <omoniyiomotoso@gmail.com>
2022-06-15 09:27:22 +0200

    dimension specification on wider assets

--- app.js
+++ app.js
@@ -1,1 +1,1 @@
-formatHashTagText([cityAndCountry.cityName, cityAndCountry.countryName]); await sendTextOnlyTweet(fullMessage); await sendTextAndMediaTweet(fullMessage, imageBuffer); Wed May 25 13:37:04 2022 +0200
+formatHashTagText([cityAndCountry.cityName, cityAndCountry.countryName]); await sendTextOnlyTweet(fullMessage); await sendTextAndMediaTweet(fullMessage, imageBuffer); Wed Jun 15 09:27:22 2022 +0200
