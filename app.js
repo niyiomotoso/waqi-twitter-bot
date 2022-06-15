ccb9282e69d3dbc476daa4b6cecdcb32884a3970
Niyi Omotoso <omoniyiomotoso@gmail.com>
2022-06-15 09:39:16 +0200

    identify countries by their woeId

--- app.js
+++ app.js
@@ -1,1 +1,1 @@
-formatHashTagText([cityAndCountry.cityName, cityAndCountry.countryName]); await sendTextOnlyTweet(fullMessage); await sendTextAndMediaTweet(fullMessage, imageBuffer); Wed Jun 15 10:08:47 2022 +0200
+formatHashTagText([cityAndCountry.cityName, cityAndCountry.countryName]); await sendTextOnlyTweet(fullMessage); await sendTextAndMediaTweet(fullMessage, imageBuffer); Wed Jun 15 09:39:16 2022 +0200
