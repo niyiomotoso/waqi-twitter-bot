5d785ace1a5ac298b8c61f3e0fbf1133bcc0fe62
Niyi Omotoso <omoniyiomotoso@gmail.com>
2022-06-15 09:58:03 +0200

    Merge pull request #456:  optimizations

--- app.js
+++ app.js
@@ -1,1 +1,1 @@
-formatHashTagText([cityAndCountry.cityName, cityAndCountry.countryName]); await sendTextOnlyTweet(fullMessage); await sendTextAndMediaTweet(fullMessage, imageBuffer); Wed Jun 15 09:31:56 2022 +0200
+formatHashTagText([cityAndCountry.cityName, cityAndCountry.countryName]); await sendTextOnlyTweet(fullMessage); await sendTextAndMediaTweet(fullMessage, imageBuffer); Wed Jun 15 09:58:03 2022 +0200
