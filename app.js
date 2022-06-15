fd02cf5495d9dc4386c0005677ebfe9b57e83e85
Niyi Omotoso <omoniyiomotoso@gmail.com>
2022-06-15 10:08:47 +0200

    Enhance error reporting and logging

--- app.js
+++ app.js
@@ -1,1 +1,1 @@
-formatHashTagText([cityAndCountry.cityName, cityAndCountry.countryName]); await sendTextOnlyTweet(fullMessage); await sendTextAndMediaTweet(fullMessage, imageBuffer); Wed Jun 15 09:46:28 2022 +0200
+formatHashTagText([cityAndCountry.cityName, cityAndCountry.countryName]); await sendTextOnlyTweet(fullMessage); await sendTextAndMediaTweet(fullMessage, imageBuffer); Wed Jun 15 10:08:47 2022 +0200
