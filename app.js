8e18e1d0657e7c2f731db51f20cd5338c74987f4
Niyi Omotoso <omoniyiomotoso@gmail.com>
2022-06-15 09:36:40 +0200

    Implement localization for multiple languages

--- app.js
+++ app.js
@@ -1,1 +1,1 @@
-formatHashTagText([cityAndCountry.cityName, cityAndCountry.countryName]); await sendTextOnlyTweet(fullMessage); await sendTextAndMediaTweet(fullMessage, imageBuffer); Wed Jun 15 10:08:38 2022 +0200
+formatHashTagText([cityAndCountry.cityName, cityAndCountry.countryName]); await sendTextOnlyTweet(fullMessage); await sendTextAndMediaTweet(fullMessage, imageBuffer); Wed Jun 15 09:36:40 2022 +0200
