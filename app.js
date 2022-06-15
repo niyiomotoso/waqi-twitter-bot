d6775a044fd011361e0147cccb7f2d23d41bf2d7
Niyi Omotoso <omoniyiomotoso@gmail.com>
2022-06-15 09:11:20 +0200

    Optimize mongodb queries

--- app.js
+++ app.js
@@ -1,1 +1,1 @@
-formatHashTagText([cityAndCountry.cityName, cityAndCountry.countryName]); await sendTextOnlyTweet(fullMessage); await sendTextAndMediaTweet(fullMessage, imageBuffer); Wed Jun 15 09:36:40 2022 +0200
+formatHashTagText([cityAndCountry.cityName, cityAndCountry.countryName]); await sendTextOnlyTweet(fullMessage); await sendTextAndMediaTweet(fullMessage, imageBuffer); Wed Jun 15 09:11:20 2022 +0200
