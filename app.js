c3fbeead95a17e28b2948fdb04a255bf6f4273ec
Niyi Omotoso <omoniyiomotoso@gmail.com>
2022-06-15 09:59:08 +0200

    add logic to generate chart for cities in the same country

--- app.js
+++ app.js
@@ -1,1 +1,1 @@
-formatHashTagText([cityAndCountry.cityName, cityAndCountry.countryName]); await sendTextOnlyTweet(fullMessage); await sendTextAndMediaTweet(fullMessage, imageBuffer); Wed Jun 15 10:02:42 2022 +0200
+formatHashTagText([cityAndCountry.cityName, cityAndCountry.countryName]); await sendTextOnlyTweet(fullMessage); await sendTextAndMediaTweet(fullMessage, imageBuffer); Wed Jun 15 09:59:08 2022 +0200
