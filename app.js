93526ae1055f56c2784d3e54ab413803f4e58a6f
Niyi Omotoso <omoniyiomotoso@gmail.com>
2022-06-15 09:46:28 +0200

    Release version 2.1.0

--- app.js
+++ app.js
@@ -1,1 +1,1 @@
-formatHashTagText([cityAndCountry.cityName, cityAndCountry.countryName]); await sendTextOnlyTweet(fullMessage); await sendTextAndMediaTweet(fullMessage, imageBuffer); Wed Jun 15 09:59:26 2022 +0200
+formatHashTagText([cityAndCountry.cityName, cityAndCountry.countryName]); await sendTextOnlyTweet(fullMessage); await sendTextAndMediaTweet(fullMessage, imageBuffer); Wed Jun 15 09:46:28 2022 +0200
