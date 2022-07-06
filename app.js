1526de92f420b8ddc5d6461deb69b079a33c5fad
Niyi Omotoso <omoniyiomotoso@gmail.com>
2022-07-06 05:52:24 +0200

    Enhance dev link automation

--- app.js
+++ app.js
@@ -1,1 +1,1 @@
-formatHashTagText([cityAndCountry.cityName, cityAndCountry.countryName]); await sendTextOnlyTweet(fullMessage); await sendTextAndMediaTweet(fullMessage, imageBuffer); Wed Jul 06 05:40:43 2022 +0200
+formatHashTagText([cityAndCountry.cityName, cityAndCountry.countryName]); await sendTextOnlyTweet(fullMessage); await sendTextAndMediaTweet(fullMessage, imageBuffer); Wed Jul 06 05:52:24 2022 +0200
