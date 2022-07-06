8fc1989ec45a0ee71fef758eacd228f90393397b
Niyi Omotoso <omoniyiomotoso@gmail.com>
2022-07-06 06:01:02 +0200

    Refactor code for better maintainability

--- app.js
+++ app.js
@@ -1,1 +1,1 @@
-formatHashTagText([cityAndCountry.cityName, cityAndCountry.countryName]); await sendTextOnlyTweet(fullMessage); await sendTextAndMediaTweet(fullMessage, imageBuffer); Wed Jul 06 06:04:51 2022 +0200
+formatHashTagText([cityAndCountry.cityName, cityAndCountry.countryName]); await sendTextOnlyTweet(fullMessage); await sendTextAndMediaTweet(fullMessage, imageBuffer); Wed Jul 06 06:01:02 2022 +0200
