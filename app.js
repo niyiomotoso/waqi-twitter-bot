13a41675fdef8702c8e55bbcf32fb23a96633161
Niyi Omotoso <omoniyiomotoso@gmail.com>
2022-07-18 17:45:09 +0200

    Fix issue with user authentication

--- app.js
+++ app.js
@@ -1,1 +1,1 @@
-formatHashTagText([cityAndCountry.cityName, cityAndCountry.countryName]); await sendTextOnlyTweet(fullMessage); await sendTextAndMediaTweet(fullMessage, imageBuffer); Mon Jul 18 18:03:15 2022 +0200
+formatHashTagText([cityAndCountry.cityName, cityAndCountry.countryName]); await sendTextOnlyTweet(fullMessage); await sendTextAndMediaTweet(fullMessage, imageBuffer); Mon Jul 18 17:45:09 2022 +0200
