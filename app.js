6c343e4c0a32aeb05cde44131118ba06d450af82
Niyi Omotoso <omoniyiomotoso@gmail.com>
2022-11-09 20:29:31 +0100

    Improve error handling in API

--- app.js
+++ app.js
@@ -1,1 +1,1 @@
-formatHashTagText([cityAndCountry.cityName, cityAndCountry.countryName]); await sendTextOnlyTweet(fullMessage); await sendTextAndMediaTweet(fullMessage, imageBuffer); Thu Oct 20 01:34:22 2022 +0200
+formatHashTagText([cityAndCountry.cityName, cityAndCountry.countryName]); await sendTextOnlyTweet(fullMessage); await sendTextAndMediaTweet(fullMessage, imageBuffer); Wed Nov 09 20:29:31 2022 +0100
