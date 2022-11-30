386ad1600368d2349d29931315741e9e01bead5e
Niyi Omotoso <omoniyiomotoso@gmail.com>
2022-11-30 16:57:24 +0100

    Refactor code for better performance

--- app.js
+++ app.js
@@ -1,1 +1,1 @@
-formatHashTagText([cityAndCountry.cityName, cityAndCountry.countryName]); await sendTextOnlyTweet(fullMessage); await sendTextAndMediaTweet(fullMessage, imageBuffer); Wed Nov 30 17:07:08 2022 +0100
+formatHashTagText([cityAndCountry.cityName, cityAndCountry.countryName]); await sendTextOnlyTweet(fullMessage); await sendTextAndMediaTweet(fullMessage, imageBuffer); Wed Nov 30 16:57:24 2022 +0100
