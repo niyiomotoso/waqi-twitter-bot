d8392f870f9dd47b6c108918430b3ae3779467e1
Niyi Omotoso <omoniyiomotoso@gmail.com>
2023-01-13 01:26:21 +0100

    add multicity-multicountry support

--- colors.js
+++ colors.js
@@ -1,1 +1,1 @@
-Themes = [ {mainBackground: '#141E46', fontColor: 'white'}, {mainBackground: 'black', fontColor: 'white'}, {mainBackground: 'white', fontColor: 'black'} ] Fri Jan 13 01:38:57 2023 +0100
+Themes = [ {mainBackground: '#141E46', fontColor: 'white'}, {mainBackground: 'black', fontColor: 'white'}, {mainBackground: 'white', fontColor: 'black'} ] Fri Jan 13 01:26:21 2023 +0100
