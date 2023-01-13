a05bf85c521de4b931232f2b1aab97c01199e28f
Niyi Omotoso <omoniyiomotoso@gmail.com>
2023-01-13 02:04:30 +0100

    Add localization support for multiple languages

--- colors.js
+++ colors.js
@@ -1,1 +1,1 @@
-Themes = [ {mainBackground: '#141E46', fontColor: 'white'}, {mainBackground: 'black', fontColor: 'white'}, {mainBackground: 'white', fontColor: 'black'} ] Fri Jan 13 02:00:25 2023 +0100
+Themes = [ {mainBackground: '#141E46', fontColor: 'white'}, {mainBackground: 'black', fontColor: 'white'}, {mainBackground: 'white', fontColor: 'black'} ] Fri Jan 13 02:04:30 2023 +0100
