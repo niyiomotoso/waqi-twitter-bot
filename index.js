112b0a05a6312a4d08a64d6515d8831b07ed7c30
Niyi Omotoso <omoniyi.omotoso@westwing.de>
2023-08-26 20:32:04 +0200

    refactor entry code

--- index.js
+++ index.js
@@ -1,27 +1,10 @@
-import {TwitterApi} from 'twitter-api-v2';
-import fetch from 'node-fetch';
 import {createCanvas} from 'canvas';
 import {BarController, BarElement, CategoryScale, Chart, LinearScale, Title} from 'chart.js';
+import {getTwitterClient} from "./services/twitterApi.js";
+import {getAirQualityByCity} from "./services/aqicnApi.js";
 
 // Register the necessary components
 Chart.register(CategoryScale, LinearScale, BarElement, Title, BarController);
-
-
-// Instantiate with desired auth type (here's Bearer v2 auth)
-const twitterClient = new TwitterApi({appKey: 'DipLrCP945optHSHiMbC1tbEv',
-    appSecret: 'tSlTN2m62wqjIi4tA4JZOS8ErKXGBUMZwAzYxOpBRXRWg1c1AX',
-    accessToken: '1695095041293783040-wD3RHThGOewTURf75DreOWw4e1b2iu',
-    accessSecret: '8Cm9KWXwURlQt0gLp1Ejws40fvSqGxwltBLi3qkLjmZtR'});
-
-// Tell typescript it's a readonly app
-const client = twitterClient.readOnly;
-
-// Function to get air quality data
-async function getAirQuality(city) {
-    const response = await fetch(`https://api.waqi.info/feed/${city}/?token=f4523891710538bf622648db0952c2e7e660e770`);
-    const data = await response.json();
-    return data.data.aqi;
-}
 
 // Create a chart and return a base64 image
 async function createChartImage(city, airQuality) {
@@ -54,7 +37,7 @@ async function tweetAirQualityChart(city, airQuality) {
     const imageBuffer = await createChartImage(city, airQuality);
 
     const tweetText = `Air quality index for ${city}: ${airQuality}`;
-
+    const client = getTwitterClient();
     // Upload the chart image to Twitter
     const mediaResponse = await client.v1.uploadMedia(imageBuffer, {
         media_category: 'tweet_image',
@@ -72,8 +55,8 @@ async function main() {
     const cities = ['New York']; // Add more cities as needed
 
     for (const city of cities) {
-        const airQuality = await getAirQuality(city);
-        await tweetAirQualityChart(city, airQuality);
+        const airQuality = await getAirQualityByCity(city);
+        await tweetAirQualityChart(city, airQuality.aqi);
     }
 }
 
