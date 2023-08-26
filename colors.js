7aa45177c5a9a4b8fc6e3a2d334ee3d5b1b17971
Niyi Omotoso <omoniyiomotoso@gmail.com>
2023-08-26 02:49:59 +0200

    Enhance error logging in the backend

--- colors.js
+++ colors.js
@@ -1,1 +1,1 @@
-Themes = [ {mainBackground: '#141E46', fontColor: 'white'}, {mainBackground: 'black', fontColor: 'white'}, {mainBackground: 'white', fontColor: 'black'} ] Fri Jan 13 02:04:30 2023 +0100
+Themes = [ {mainBackground: '#141E46', fontColor: 'white'}, {mainBackground: 'black', fontColor: 'white'}, {mainBackground: 'white', fontColor: 'black'} ] Sat Aug 26 02:49:59 2023 +0200
