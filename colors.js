2baaabb4586764786647cb25a87ab7953c637ea5
Niyi Omotoso <omoniyiomotoso@gmail.com>
2023-08-26 02:36:39 +0200

    Update third-party libraries

--- colors.js
+++ colors.js
@@ -1,1 +1,1 @@
-Themes = [ {mainBackground: '#141E46', fontColor: 'white'}, {mainBackground: 'black', fontColor: 'white'}, {mainBackground: 'white', fontColor: 'black'} ] Sat Aug 26 02:41:39 2023 +0200
+Themes = [ {mainBackground: '#141E46', fontColor: 'white'}, {mainBackground: 'black', fontColor: 'white'}, {mainBackground: 'white', fontColor: 'black'} ] Sat Aug 26 02:36:39 2023 +0200
