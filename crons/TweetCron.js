4e8d70aec4686e7c52eb51faf5d9d955b764e517
Niyi Omotoso <omoniyi.omotoso@westwing.de>
2023-08-27 06:09:17 +0200

    change tweet interval to every 15 minutes

--- crons/TweetCron.js
+++ crons/TweetCron.js
@@ -1,7 +1,7 @@
 import cron from "node-cron"
 import { SingleCityTweetMain } from "../workers/SingeCitytweet.js";
 
-const runSingleCityTweet = cron.schedule('*/5 * * * *', async () => {
+const runSingleCityTweet = cron.schedule('*/15 * * * *', async () => {
     console.log("running runSingleCityTweet cron")
     await SingleCityTweetMain();
 });
