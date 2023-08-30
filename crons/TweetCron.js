791620268d4a7e9ca629d29c32585632ebf01854
Niyi Omotoso <omoniyiomotoso@gmail.com>
2023-08-30 12:19:40 +0100

    fix cron schedule

--- crons/TweetCron.js
+++ crons/TweetCron.js
@@ -9,12 +9,12 @@ const runSingleCityTweet = cron.schedule('*/15 * * * *', async () => {
     await SingleCityTweetMain();
 });
 
-const runMultiCitySameCountryTweet = cron.schedule('* */1 * * *', async () => {
+const runMultiCitySameCountryTweet = cron.schedule('0 */1 * * *', async () => {
     console.log("running runMultiCitySameCountryTweet cron")
     await MultiCitySameCountryMain();
 });
 
-const runMultiCityMultiCountryTweet = cron.schedule('* */2 * * *', async () => {
+const runMultiCityMultiCountryTweet = cron.schedule('0 */2 * * *', async () => {
     console.log("running runMultiCityMultiCountryTweet cron")
     await MultiCityMultiCountryMain();
 });
