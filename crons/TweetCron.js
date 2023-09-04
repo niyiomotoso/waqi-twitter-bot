f49bf84ce3d9189e03737a858995894a919c118d
Niyi Omotoso <omoniyiomotoso@gmail.com>
2023-09-04 13:38:19 +0100

    general start jobs for longer

--- crons/TweetCron.js
+++ crons/TweetCron.js
@@ -4,17 +4,17 @@ import { deleteAllTweets } from "../services/DailyTweetService.js";
 import {MultiCitySameCountryMain} from "../workers/MultiCitySameCountryTweet.js";
 import {MultiCityMultiCountryMain} from "../workers/MultiCityMultiCountry.js";
 
-const runSingleCityTweet = cron.schedule('*/23 * * * *', async () => {
+const runSingleCityTweet = cron.schedule('*/41 * * * *', async () => {
     console.log("running runSingleCityTweet cron")
     await SingleCityTweetMain();
 });
 
-const runMultiCitySameCountryTweet = cron.schedule('3 */2 * * *', async () => {
+const runMultiCitySameCountryTweet = cron.schedule('3 */3 * * *', async () => {
     console.log("running runMultiCitySameCountryTweet cron")
     await MultiCitySameCountryMain();
 });
 
-const runMultiCityMultiCountryTweet = cron.schedule('11 */3 * * *', async () => {
+const runMultiCityMultiCountryTweet = cron.schedule('11 */5 * * *', async () => {
     console.log("running runMultiCityMultiCountryTweet cron")
     await MultiCityMultiCountryMain();
 });
