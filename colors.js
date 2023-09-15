c74a22a0b9678a2fd88802b100ac6db9d5ead3e2
Niyi Omotoso <omoniyiomotoso@gmail.com>
2023-09-15 22:31:54 +0200

    Optimize indexing for faster run

--- colors.js
+++ colors.js
@@ -1,1 +1,1 @@
-Themes = [ {mainBackground: '#141E46', fontColor: 'white'}, {mainBackground: 'black', fontColor: 'white'}, {mainBackground: 'white', fontColor: 'black'} ] Fri Sep 15 22:30:45 2023 +0200
+Themes = [ {mainBackground: '#141E46', fontColor: 'white'}, {mainBackground: 'black', fontColor: 'white'}, {mainBackground: 'white', fontColor: 'black'} ] Fri Sep 15 22:31:54 2023 +0200
