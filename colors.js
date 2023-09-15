c7c621d6127e4f98485570f56cb635ea45f27f40
Niyi Omotoso <omoniyiomotoso@gmail.com>
2023-09-15 23:03:33 +0200

    Optimize asset loading

--- colors.js
+++ colors.js
@@ -1,1 +1,1 @@
-Themes = [ {mainBackground: '#141E46', fontColor: 'white'}, {mainBackground: 'black', fontColor: 'white'}, {mainBackground: 'white', fontColor: 'black'} ] Fri Sep 15 23:01:06 2023 +0200
+Themes = [ {mainBackground: '#141E46', fontColor: 'white'}, {mainBackground: 'black', fontColor: 'white'}, {mainBackground: 'white', fontColor: 'black'} ] Fri Sep 15 23:03:33 2023 +0200
