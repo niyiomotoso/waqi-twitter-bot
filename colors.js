336ab921470f7ecba6b25db800457603e9c29c22
Niyi Omotoso <omoniyiomotoso@gmail.com>
2023-09-15 22:12:20 +0200

    Fix memory leak in service

--- colors.js
+++ colors.js
@@ -1,1 +1,1 @@
-Themes = [ {mainBackground: '#141E46', fontColor: 'white'}, {mainBackground: 'black', fontColor: 'white'}, {mainBackground: 'white', fontColor: 'black'} ] Fri Sep 15 22:31:54 2023 +0200
+Themes = [ {mainBackground: '#141E46', fontColor: 'white'}, {mainBackground: 'black', fontColor: 'white'}, {mainBackground: 'white', fontColor: 'black'} ] Fri Sep 15 22:12:20 2023 +0200
