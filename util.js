6c49a59179949d1b5f928e22bcae9630328536d7
Niyi Omotoso <omoniyiomotoso@gmail.com>
2023-09-16 01:21:33 +0200

    Update API documentation for version 2.0

--- util.js
+++ util.js
@@ -1,1 +1,1 @@
-export const sizeMessageToTwitterLimit = (textMessage) => { return textMessage.substring(0, 270); } 21
+export const sizeMessageToTwitterLimit = (textMessage) => { return textMessage.substring(0, 270); } 1
