a157bef65c91ca40bb911d5a71f0189db27ab10e
Niyi Omotoso <omoniyiomotoso@gmail.com>
2023-10-10 01:27:25 +0200

    revert changes

--- util.js
+++ util.js
@@ -1,1 +1,1 @@
-export const sizeMessageToTwitterLimit = (textMessage) => { return textMessage.substring(0, 270); } 11
+export const sizeMessageToTwitterLimit = (textMessage) => { return textMessage.substring(0, 270); } 1
